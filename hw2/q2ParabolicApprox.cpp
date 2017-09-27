788448cb7386ce5d0f2d41222f8636a2c441957c
nemimccarter <nemimccarter@gmail.com>
2017-09-26 18:53:49 -0700

    begin question 2. safety commit

---
+++ hw2/q2ParabolicApprox.cpp
@@ -0,0 +1,31 @@
+// Nehemya McCarter-Ribakoff
+// Dujmovic
+// HW2
+// 29 September 2017
+
+#include <iostream>
+// y = ax^2 + bx + c
+
+int main() {
+
+
+return 0;
+}
+
+void approximateCurve(int x1, int y1, int x2, int y2, 
+                      int x3, int y3, int x) {
+
+    /*
+    y1 = ax1^2 + bx1 + c
+    y2 = ax2^2 + bx2 + c
+    y3 = ax3^2 + bx3 + c
+    
+
+    ax1^2 = y1 - bx1 + c
+    a = (y1 - bx1 + c) / x1^2
+    
+    bx1 = y1 - ax1^2 - c
+    c = y1 - ax1^2 - bx1
+    */
+
+}
---
+++ hw2/q4BigInt.cpp
@@ -0,0 +1,23 @@
+// Nehemya McCarter-Ribakoff
+// Dujmovic
+// HW2
+// 29 September 2017
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
+
+bigInt(int n) {
+    
+}
+
+void printOne() {
+    for (int row = 0; row < 6; row++ {
+        for (int column = 0; column < 6; column++) {
+        
+        }
+    }
+}
