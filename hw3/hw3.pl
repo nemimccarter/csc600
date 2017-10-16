b1c68a54adde8cef7c45e6d78a5762fe2b42c3a2
nemimccarter <nemimccarter@gmail.com>
2017-10-16 08:59:37 -0700

    finish .pl. Continue printout formatting

--- hw3/hw3.pl
+++ hw3/hw3.pl
@@ -13,7 +13,8 @@ parent(P, C)    :- family([P, _, Children]), member(C, Children); family([_, P, 
 father(F, C)    :- male(F), parent(F, C).
 mother(M, C)    :- female(M), parent(M, C).
 greatgrandparent(GG, C) :- parent(GG, GP), parent(GP, P), parent(P, C).
-ancestor(A, C)  :- parent(A, C); parent(A, P), ancestor(A, P).
+ancestor(A, C)  :- parent(A, C).
+ancestor(A, C)  :- parent(P, C), ancestor(A, P).
 
 siblings1(S, C) :- family([_, _, Children]), member(S, Children), member(C, Children).
 siblings2(C, S) :- family([_, _, Children]), member(C, Children), member(S, Children).
@@ -34,3 +35,63 @@ granddaughter(GD, G) :- grandchild(GD, G), female(GD).
 
 is_member(X, L) :- member(X, L).
 is_head(X, L) :- [H|_] = L, H = X.
+last_element([X], X).
+last_element([_|T], X) :- last_element(T, X).
+adjacent2(X,Y,[X,Y|_]).
+adjacent2(X,Y,[_|T]) :- adjacent2(X,Y,T).
+
+adjacent3(X,Y,Z,[X,Y,Z|_]).
+adjacent3(X,Y,Z,[_|T]) :- adjacent3(X,Y,Z,T).
+
+append1([],L,L).
+append1([X|T1],L2,[X|T3]) :- append1(T1,L2,T3).
+
+delete1(X,[X|T],T).
+delete1(X,[H,T],[H,T1]) :- delete1(X,T,T1).
+
+append2([],E,[E]).
+append2([X|T1],E2,[X|T3]) :- append2(T1,E2,T3).
+
+insert1(X,L,XL) :- delete1(X,XL,L).
+
+length1([],0).
+length1([_|T],N) :- length1(T,N1), N is 1 + N1.
+
+reverse1([],[]).
+reverse1([H|T],X) :- reverse1(T,Y), append2(Y,H,X).
+
+palindrome(L1) :- reverse1(L1,X), X=L1.
+
+display1([]) :- nl.
+display1([H|T]) :- write(H), tab(1), display1(T).
+
+
+/*
+	nocheck(X/Y,L) takes a queen X/Y and a list of queens. We succeed if and only if the
+	X/Y queen holds none of the others in check. 
+*/
+nocheck(_,[]).
+nocheck(X/Y,[X1/Y1 | Rest]) :- 
+	X=\=X1, 
+	Y=\=Y1, 
+	abs(Y1-Y) =\= abs(X1-X), 
+	nocheck(X/Y,Rest).
+	
+/*
+	legal(L) succeeds if L is a legal placement of queens: all coordinates in range and 
+	no queen in check.
+*/
+legal([]).
+legal([X/Y | Rest]) :-
+	legal(Rest),
+	member(X,[1,2,3,4,5,6,7,8]),
+	member(Y,[1,2,3,4,5,6,7,8]),
+	nocheck(X/Y, Rest).
+	
+/*
+	eightqueens(X) succeeds if X is a legal placement of eight queens, listed in order
+	of their X coordinates
+*/
+eightqueens(X) :-
+	X = [1/_,2/_,3/_,4/_,5/_,6/_,7/_,8/_],
+	legal(X).
